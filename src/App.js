af3c108dafb415aae3ea9e55f99c7d053a394939
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-08-01 17:25:47 +0300

    Header is visible

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@
 */
 
 // State hook u import edin
-import React from "react";
+import React, { useState } from "react";
+import Gonderiler from "./bilesenler/Gonderiler/Gonderiler";
+import AramaCubugu from "./bilesenler/AramaCubugu/AramaCubugu";
+import sahteVeri from "./sahte-veri";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
 import "./App.css";
 
 const App = () => {
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [aramaKriteri, setAramaKriteri] = useState();
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
 
   const gonderiyiBegen = (gonderiID) => {
+    setGonderiler(
+      gonderiler.map((gonderi) => {
+        return gonderi.id === gonderiID ? "???" : gonderi;
+      })
+    );
+
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
@@ -32,8 +43,8 @@ const App = () => {
 
   return (
     <div className="App">
-      App Çalışıyor
-      {/* Yukarıdaki metni projeye başladığınızda silin*/}
+      <AramaCubugu />
+      <Gonderiler gonderiler={gonderiler} gonderiyiBegen={gonderiyiBegen} />
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
--- src/bilesenler/Gonderiler/Gonderiler.js
+++ src/bilesenler/Gonderiler/Gonderiler.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Gonderi from "./Gonderi";
 import "./Gonderiler.css";
-
 const Gonderiler = (props) => {
   // 🔥 Gönderiler'in ebeveyninin doğru değişkenleri doğru şekilde ilettiğine emin olun!
   const { gonderiyiBegen, gonderiler } = props;
 
+  const a = gonderiler.map((gonderi) => {
+    console.log(gonderi);
+  });
+
   return (
     <div className="posts-container-wrapper">
+      <div>Çıktı: {}</div>
       {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/}
       {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
     </div>
